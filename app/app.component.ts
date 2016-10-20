a062c18440b3d1e4122f461397c7541eceff430e
Lisa MacCarrigan and Meredith Alcorn <meredith@>
2016-10-20 11:45:05 -0400

    add NaturalRemedy class and constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  public naturalremedy: NaturalRemedy[] = [
+    new NaturalRemedy("Nux Vomica", "Tree seed", 9.99, ["asthma", "rheumatism", "hemorrhoids"]),
+    new NaturalRemedy("Rosehip Seed Oil", "Fruit", 19.99, ["reduces wrinkles", "eczema", "minimize appearance of scars"]),
 
+  ];
+}
+
+export class NaturalRemedy {
+  public selected: boolean = false;
+  constructor(public name: string, public source: string, public price: number, public ailment: string[]) {   }
 }
